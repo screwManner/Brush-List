9b96c756d42b3af89cbcce49becb968a28956058
邓诚杰 <1114764650@qq.com>
2024-07-30 14:42:48 +0800

    feat:新增小说菜单和iframe嵌入功能

--- src/App.tsx
+++ src/App.tsx
@@ -1,24 +1,63 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React,{ useEffect, useState } from "react";
+// import logo from './logo.svg';
+import "./App.css";
+import type { MenuProps } from "antd";
+import { Menu } from "antd";
+import WebList from './utils'
+
+type MenuItem = Required<MenuProps>["items"][number];
+
+const isIframe = (input: HTMLElement | null): input is HTMLIFrameElement =>
+  input !== null && input.tagName === 'IFRAME';
 
 function App() {
+
+  const [items,setItems] = useState<MenuItem[]>([]);
+  const [iframeSrc,setIframeSrc] = useState<string>()
+
+  useEffect(()=>{
+    // 处理左侧菜单数据
+    const temporarilyList = [...WebList];
+    const resultList = temporarilyList.map((item)=>{
+      return {
+        ...item,
+        label:item.name
+      }
+    })
+    setItems(resultList)
+  },[])
+
+  const onClick: MenuProps['onClick'] = (e) => {
+    const temporarilyData = WebList.filter((item)=>{
+      return item.key === e.key
+    })
+    console.log('处理后的数组',temporarilyData)
+    setIframeSrc(temporarilyData[0].address);
+    // document.getElementById('myframe')?.contentWindow?.location.reload(true);
+  };
+
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
       </header>
+      <body className="App-body">
+        <div className="left-menu">
+          <Menu
+            onClick={onClick}
+            style={{ width: 256 }}
+            defaultSelectedKeys={["1"]}
+            defaultOpenKeys={["sub1"]}
+            mode="inline"
+            items={items}
+          />
+        </div>
+        <div className="right-content">
+          {/* <div className="top-menu"></div>
+          <div className="right-container"></div> */}
+          {/* <p>网站地址{iframeSrc}</p> */}
+          <iframe width={'100%'} height={'98%'} src={iframeSrc} title="网站" id="myIframe" frameBorder={0}></iframe>
+        </div>
+      </body>
     </div>
   );
 }
--- src/index.tsx
+++ src/index.tsx
@@ -1,15 +1,18 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
-import App from './App';
 import reportWebVitals from './reportWebVitals';
+import { RouterProvider } from 'react-router-dom';
+import route from './router/index'
+
 
 const root = ReactDOM.createRoot(
   document.getElementById('root') as HTMLElement
 );
+
 root.render(
   <React.StrictMode>
-    <App />
+    <RouterProvider router={route}></RouterProvider>
   </React.StrictMode>
 );
 
